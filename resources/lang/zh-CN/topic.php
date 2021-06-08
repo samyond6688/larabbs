81198be4c7beda5a2cc92fb36088a689f3cf98e9
vagrant <vagrant@homestead>
2021-06-08 06:32:50 +0000

    初始化

---
+++ resources/lang/zh-CN/topic.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'labels' => [
+        'Topic' => '贴子',
+        'topic' => '贴子',
+    ],
+    'fields' => [
+        'title' => '标题',
+        'body' => '内容',
+        'reply_count' => '回复数量',
+        'view_count' => '访问数',
+        'slug' => 'slug',
+    ],
+    'options' => [
+    ],
+];
---
+++ resources/lang/zh-CN/user.php
@@ -0,0 +1,19 @@
+<?php 
+return [
+    'labels' => [
+        'User' => 'User',
+        'user' => 'User',
+    ],
+    'fields' => [
+        'name' => '账号',
+        'email' => '邮箱',
+        'email_verified_at' => '邮箱激活时间',
+        'password' => '密码',
+        'avatar' => '头像',
+        'introduction' => '介绍',
+        'notification_count' => '未读消息数',
+        'last_actived_at' => '最后活跃时间',
+    ],
+    'options' => [
+    ],
+];
